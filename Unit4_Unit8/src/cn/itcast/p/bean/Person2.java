8ec21a84540755bbeb7c5cc57ac2cfa5be24f16e
echoiir <echoiir@outlook.com>
2020-01-05 16:59:54 +0800

    Update

---
+++ Unit4_Unit8/src/cn/itcast/p/bean/Person2.java
@@ -0,0 +1,94 @@
+package src.cn.itcast.p.bean;
+
+import java.util.Objects;
+// 为ThreeSet设计的Person类，
+// 1、实现Comparable接口
+// 2、并覆写compareTo方法
+public class Person2 /*extends Object*/ implements Comparable{
+	private int age;
+	private String name;
+
+	public Person2() {
+		super();
+	}
+
+	public Person2(int age, String name) {
+		super();
+		this.age = age;
+		this.name = name;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		System.out.println("================ Running equals ============");
+		boolean result;
+		
+		if (obj == this)
+			result= true;
+		if (!(obj instanceof Person2)) {
+			// ====== object instanceof class
+			// 如果object 是 class 的一个实例，则 instanceof 运算符返回 true。
+			// 如果 object 不是指定类的一个实例，或者 object 是 null，则返回 false。
+			// 
+			result= false; // obj不是Person2的实例，则必然this!=obj,此时obj.equals(this)为false,即result= false; 
+		}
+		Person2 person = (Person2) obj;
+		result = (this.age == person.age && Objects.equals(this.name, person.name));
+		System.out.println("this:" + this +"\tobj:" + obj + "\n"+ "this equals obj:\t"  + result);
+		return result;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		/*==== return：从小到大排序。
+	-1或者负数：this<o，o排在this之后
+	 1或者正数：this>o，o排在this之前
+			 0：this=o, 集合中就不插入o了
+		*/  
+
+		// ======= 按年龄排序
+		Person2 p = (Person2) o;
+		// =============== 写法一 =============
+		// System.out.println("\n按年龄排序:");
+		// if (this.age == p.getAge()){
+		// 	if(this.name == p.getName()){
+		// 		return 0; // 所有对象是一样的，如果是集合的话，后续的对象就不能添加进去。
+		// 	}else{
+		// 		// return 1;
+		// 		return this.name.compareTo(p.getName());
+		// 	}
+		// }else if (this.age>p.getAge()){
+		// 	return 1;//新的年龄小于原来的年龄，排在当前的后面
+		// }else {
+		// 	return -1;
+		// }	
+		
+		// ============= 写法二 ==================
+		// System.out.println("\n按年龄排序:");
+		// int temp = this.age - p.getAge();	
+		// return temp ==0 ? this.name.compareTo(p.getName()):temp;
+		
+		// 按姓名排序
+		// System.out.println("\n按姓名排序:");
+		int temp = this.name.compareTo(p.getName());	
+		return temp ==0 ? (this.age-p.getAge()):temp;
+
+
+	}
+}
--- Unit4_Unit8/src/cn/itcast/p6/treeset/demo/TreeSetDemo.java
+++ Unit4_Unit8/src/cn/itcast/p6/treeset/demo/TreeSetDemo.java
@@ -3,20 +3,41 @@ package src.cn.itcast.p6.treeset.demo;
 import java.util.Iterator;
 import java.util.TreeSet;
 
+
+import src.cn.itcast.p.bean.Person2;
+
 class TreeSetDemo{
     public static void main(String[] args) {
-        TreeSet ts = new TreeSet();
+        // ====== 一、TreeSet存字符串 ==============
+        // TreeSet ts = new TreeSet();
+        // ts.add("abc");
+        // ts.add("aa");
+        // ts.add("nba");
+        // ts.add("cba");
 
-        ts.add("abc");
-        ts.add("aa");
-        ts.add("nba");
-        ts.add("cba");
+        // Iterator it = ts.iterator();
 
-        Iterator it = ts.iterator();
+        // while(it.hasNext()){
+        //     System.out.println(it.next());
+        // }
 
-        while(it.hasNext()){
-            System.out.println(it.next());
-        }
+        // ====== 二、TreeSet存对象 ================
+            System.out.println("\n====== 集合TreeSet中存对象-取重复对象（new之后不算重复） ========");
+            TreeSet ts2 = new TreeSet(new ComparatorByName()); // 传入比较器
+            // TreeSet ts2 = new TreeSet();
+
+			System.out.println("TreeSet的add成功与否：\t"+ ts2.add(new Person2(21,"lisi1"))); // 不算重复元素
+			System.out.println("TreeSet的add成功与否：\t"+ ts2.add(new Person2(23,"lisi2"))); // 不算重复元素
+            System.out.println("TreeSet的add成功与否：\t"+ ts2.add(new Person2(28,"lisi3"))); // 不算重复元素
+            System.out.println("TreeSet的add成功与否：\t"+ ts2.add(new Person2(29,"lisi4"))); // 不算重复元素
+            System.out.println("TreeSet的add成功与否：\t"+ ts2.add(new Person2(23,"lisi5"))); // 不算重复元素
+
+			System.out.println("\n ======= 开始输出添加完对象后的TreeSet");
+			Iterator<Object> it2 = ts2.iterator();
+			while(it2.hasNext()){
+				Person2 obj = (Person2)it2.next(); 
+				System.out.println("Person.getAge: " + obj.getAge() + "\tPerson.getName: " + obj.getName() + "\t全类名：" + obj);
+			}
 
     }
 }
